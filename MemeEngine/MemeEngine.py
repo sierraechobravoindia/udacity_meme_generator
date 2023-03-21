fc760cd0ad48fea3f894ba5d32811b1f87db739e
Sebastian Ernst <sierraechobravoinidia@gmail.com>
2023-03-21 16:06:04 +0100

    Added pillow features as per request

--- MemeEngine/MemeEngine.py
+++ MemeEngine/MemeEngine.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
+from textwrap import wrap
 import random
 
 class MemeEngine():
@@ -22,7 +23,13 @@ class MemeEngine():
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype('/usr/share/fonts/truetype/tlwg/Laksaman-Bold.ttf', size=20)
         message = f"'{text}' - {author}"
-        draw.text((10, 30), message, fill='white')
+        wrapped = wrap(message, 25)
+        w, h = random.randint(5,200), random.randint(5,300)
+        text_y = h
+        for line in wrapped:
+            width, height = font.getsize(line)
+            draw.text((w, text_y), line, fill='white', font=font)
+            text_y += height
         meme_path = f"meme_{random.randint(0,1000000)}.jpg"
         img.save(meme_path)
         return meme_path
